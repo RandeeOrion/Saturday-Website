4597ef16ed473fbbccfb7b29d2e11a1dcae8ff54
Randee Orion <randeegood@gmail.com>
2019-09-26 14:57:38 -0700

    changed a wants to want

--- addNewFeedHTML.js
+++ addNewFeedHTML.js
@@ -22,13 +22,13 @@ function addNewFeedHTML(feedName, weight) {
 var wantToAddNewFeed = prompt("Do you want to add a new kind of feed?");
 
 //  Change if to while: and as long as the last question is yes, then it should contiue to run.
-if (wantToAddNewFeed === 'yes') {
+while (wantToAddNewFeed === 'yes') {
     var feedName = prompt ('What kind of feed is it?');
     var weight = prompt ('What is the weight of the bag?');
     var newFeedHTML = addNewFeedHTML(feedName, weight);
     console.log(addNewFeed);
     document.write (addNewFeed);
 
-    wantsToAddCat = prompt ('Do You want to add another bag of food?')
+    wantToAddNewFeed = prompt ('Do You want to add another bag of food?')
     // having the wantsToAddCat prompt creates an escape hatch. IF that wasn't there, then there is no way for the conditino to change and the loop to end. 
 }
